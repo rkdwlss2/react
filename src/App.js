d2bef1ba713c4f61e51930c8e71c4730aee763b7
rkdwlss2 <rkdwlss2@gmail.com>
2023-01-23 13:12:47 +0900

    useStates 사용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Sub from './Sub';
 
 function App() {
-  let list = [1, 2, 3];
+  // let list = [1, 2, 3];
+  const [number, setNumber] = useState(1); // react 안에 hooks 라이브러리 상태값이 됨
+  //let number = 1;
+  const add = () => {
+    setNumber(number + 1); // 리엑트 한테 number값 변경할께 라고 요청
+    console.log('add', number);
+  };
 
   return (
     <div>
       <div>
-        {list.map((n) => (
-          // eslint-disable-next-line react/jsx-key
-          <h1>{n}</h1>
-        ))}
+        <h1>숫자 : {number}</h1>
+        <button onClick={add}>더하기</button>
+        <Sub />
       </div>
     </div>
   );
