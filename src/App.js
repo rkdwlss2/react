3b418d08c70c49114c8267a17da47f7ae9af85a7
rkdwlss2 <rkdwlss2@gmail.com>
2023-01-28 21:15:48 +0900

    useEffect 사용해봄 ㅋ

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React, { useState } from 'react';
 import './App.css';
 
+import React, { useEffect, useState } from 'react';
+
 function App() {
-  // let list = [1, 2, 3];
-  const [num, setNum] = useState(5);
-  console.log(`App 실행됨`);
-
-  let sample = [
-    { id: 1, name: '홍길동' },
-    { id: 2, name: '임꺽정' },
-    { id: 3, name: '장보고' },
-    { id: 4, name: '코스' },
-  ];
-
-  //다운로드 받음
-  const [users, setUsers] = useState(sample);
+  const [data, setData] = useState(0);
+  const [search, setSearch] = useState(0);
 
   const download = () => {
-    // const a = sample.concat({ id: 5, name: '조자룡' });
-    // console.log(sample);
-    setUsers([...sample, { id: num, name: '조자룡' }]);
-    setNum(num + 1);
+    //다운로드 받고 (통신)
+    let downloadData = 5;
+    setData(downloadData);
   };
 
+  // 실행시점: (1) App() 함수가 최초 실행될때
+  // (2) 상태 변수가 변경될때 또 실행됨
+  useEffect(() => {
+    console.log('App() useEffect 실행됨');
+    download();
+  }, [search]);
   return (
     <div>
-      <button onClick={download}>다운로드</button>
-      {users.map((u) => (
-        <h1 key={u.id}>
-          {u.id}, {u.name}
-        </h1>
-      ))}
+      <button
+        onClick={() => {
+          setSearch(2);
+        }}
+      >
+        검색
+      </button>
+      <h1>데이터 :{data}</h1>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        더하기
+      </button>
     </div>
   );
 }
