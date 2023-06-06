45aac3290a755c84fbff76f31fed7b61e449d370
rkdwlss2 <rkdwlss2@gmail.com>
2023-06-06 15:18:39 +0900

    react test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import ListPage from './pages/ListPage';
+import WritePage from './pages/WritePage';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Route path='/' exact={true} component={ListPage} />
+      <Route path='/write' exact={true} component={WritePage} />
+
     </div>
   );
 }
